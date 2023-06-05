f4aecbcdeb28f03116c9193244eeac3e78f89aaf
Replit user <>
2023-06-05 10:18:45 -0400

    minor changes made

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-# ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template, request, redirect
+from pymongo import MongoClient
 
-
-# -- Initialization section --
+# -- INITIALIZE APP --
 app = Flask(__name__)
 
+# CONFIGURE MONGO DB
+# create the clent
+username = "user"
+password = "pass"
+url = f"your URL {username} {password}"
+client = MongoClient(url)
 
-# -- Routes section --
-@app.route('/')
-@app.route('/index')
-def index():
-    return "hello world"
+# our database manager object
+db = client["your DB name"]
+
+# CRUD Operations
+# collection = db.events
+# event = {
+# "name" : "Powerwashing my Turtle"
+# "date" : "2020-03-12"
+# }
+# CREATE
+
+# READ
+
+# UPDATE
+
+# DELETE
+
+
+# HOME PAGE
+@app.route("/")
+@app.route("/home")
+def show_board():
+  return render_template("index.html")
+
+
+# HANDLE POST AND GET for a NEW EVENT
+@app.route("/new")
+def new_event():
+  return render_template("form.html")
+
+
+# CLEAR OUT THE EVENTS
+# @app.route("/clear")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
